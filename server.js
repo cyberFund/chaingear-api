c6894de239779a0cacba9b3c8f453a13739b7315
Andrey <luzkovskyandrey@gmail.com>
2017-11-28 13:27:08 +0300

    add CORS

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const data = require('./data/chaingear.json');
 // App
 const app = express();
 app.use('/logos', express.static('logos'));
+
+//CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/tokens', (req, res) => {
 	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 	console.log(' fullUrl ', fullUrl);
